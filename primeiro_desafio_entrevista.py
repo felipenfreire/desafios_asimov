e7b6bce9fe8dd1ec2859ba855bbe8edab6df54c3
Felipe do Nascimento Freire <felipenascimento871@gmail.com>
2026-03-09 14:24:43 -0300

    primeiro commit e primeiro desafio feito

---
+++ primeiro_desafio_entrevista.py
@@ -0,0 +1,17 @@
+# Desafio - crie um programa que:
+# - Pede pelo seu nome e idade
+# - Dá oi para você
+# - Conta quantas letras seu nome possui
+# - Fala quantos anos você terá daqui a 5 anos
+
+nome = input('Digite seu primeiro nome: ')
+idade = int(input('Digite a sua idade: '))
+
+print(f'Olá, {nome}!')
+print(f'Seu nome tem {len(nome)} letras')
+print(f'Daqui a 5 anos você terá {idade + 5} anos')
+
+
+
+
+
