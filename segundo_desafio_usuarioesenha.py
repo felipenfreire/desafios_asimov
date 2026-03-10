8f42277af2c7607c91be25e56b475f5a5f3ae44a
Felipe do Nascimento Freire <felipenascimento871@gmail.com>
2026-03-09 21:31:44 -0300

    terceiro commit para melhorar o código do segundo desafio.

--- segundo_desafio_usuarioesenha.py
+++ segundo_desafio_usuarioesenha.py
@@ -15,10 +15,11 @@ senha = input('Digite a sua senha: ')
 if usuario == usuario_correto:
     if senha == senha_correta:
         print(f'Bem vindo {usuario}, login feito com sucesso :) ')
-    elif senha != senha_correta:
+    else:
         print(f'Senha incorreta para o usuário {usuario}')
 else:
     print(f'Usuário {usuario} não encontrado em nosso banco de dados ); ')
+
 
     
     
